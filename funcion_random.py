f37a49ed44dd2eef0d1d686c31e064c9fd2b7677
ricardo-ruiz <72661053+ricardo-ruiz@users.noreply.github.com>
2020-11-21 15:40:42 -0600

    ignorando archivos del entorno virtual

--- funcion_random.py
+++ funcion_random.py
@@ -1,5 +1,27 @@
-# ejecicio para funcionamiento de numeros random dentro de python
+# Generar una lista con valores aleatorios
 
-import random
+from random import randint
 
-print(random.randint(1, 1000000))
+# Lista vacia que contendra
+lista_aleatoria = []
+
+# solicitar cuantos valores
+elementos = input('Cuantos elementos necesitas? ')
+elementos = int(elementos)
+
+contador = 1
+
+while contador <= elementos:
+    # Generamos un valor aleatorio
+    matriz = randint(1, 100)
+
+    # Numero aleatorio lo multiplicamos por el numero dado por el usuario
+    valor = matriz * elementos
+
+    # Guardar valor aleatorio en la lista
+    lista_aleatoria.append(valor)
+
+    # Sumar valor a contador para la siguiente vuelta
+    contador = contador + 1
+
+print(lista_aleatoria)
