9048033feb2109534ad9aea47845fe150b678368
ricardo-ruiz <72661053+ricardo-ruiz@users.noreply.github.com>
2020-11-21 13:32:38 -0600

    added file to explain lists in python

---
+++ listas.py
@@ -0,0 +1,14 @@
+# archivo explicando listas
+
+numeros = [23, 89, 127, 1289, 8912]
+
+print(numeros)
+# agregar un elemento a una lista ocupamos append
+
+numero_extra = 33
+nunmero_extra_dos = 1223
+numero_extra_tres = 89
+
+numeros.append(numero_extra)
+print(numeros)
+
